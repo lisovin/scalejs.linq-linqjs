e6e8e4331d5d9d6dc8d2d1ea7b1ab52cc3f5fa47
Peter Lisovin <peter@lisovin.com>
2013-01-10 17:38:20 -0500

    Initial version 0.1.0

---
+++ Properties/test.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="../Scripts/require.js"/>
+/// <reference path="../Scripts/jasmine.js"/>
+
+/*global requirejs*/
+requirejs({
+    paths: {
+        'scalejs' : '../Scripts/scalejs-0.1.1',
+        'linq' : '../Scripts/linq'
+    }
+}, ['../test/all.tests']);
---
+++ test/scalejs.linq.test.js
@@ -0,0 +1,10 @@
+﻿/*global define,describe,expect,it*/
+/*jslint sloppy: true*/
+/// <reference path="../Scripts/jasmine.js"/>
+define(['../js/scalejs.linq'], function (extension) {
+    describe('scalejs.linq extension', function () {
+        it('is defined', function () {
+            expect(extension).toBeDefined();
+        });
+    });
+});
