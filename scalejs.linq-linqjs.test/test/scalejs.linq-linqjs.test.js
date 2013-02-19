b3bd716b8a630db71073fee72ba47bdb228d06d1
Peter Lisovin <peter@lisovin.com>
2013-02-19 16:36:09 -0500

    Updated to latest version of scalejs (0.2.6)

---
+++ scalejs.linq-linqjs.test/test/scalejs.linq-linqjs.test.js
@@ -0,0 +1,12 @@
+﻿/*global define,describe,expect,it*/
+/*jslint sloppy: true*/
+/// <reference path="../Scripts/jasmine.js"/>
+define([
+    'scalejs!core'
+], function (core) {
+    describe('`linq` core extension', function () {
+        it('is defined', function () {
+            expect(core.linq).toBeDefined();
+        });
+    });
+});
